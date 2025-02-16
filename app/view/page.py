d3166ac53f8edf06f268aef28f0f88c22bb18378
koesnam <76582376+10kseok@users.noreply.github.com>
2025-02-16 23:14:04 +0900

    feat: 랜딩 페이지 추가
    - 랜딩 페이지와 전자책 변환 페이지 분리
    - 서버에 nginx를 붙이면서 jinja가 필요없어짐
    - 생각보다 서버에서 던져주는 값이 없음
    - 이걸 따로 빼면 매번 따로 배포해야해서 리소스가 들어가니 우선은 지금 그대로 두는 걸로

--- app/view/page.py
+++ app/view/page.py
@@ -8,3 +8,8 @@ templates = Jinja2Templates(directory="./app/view/templates")
 @router.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/convert", response_class=HTMLResponse)
+def convert(request: Request):
+    return templates.TemplateResponse("convert.html", {"request": request})

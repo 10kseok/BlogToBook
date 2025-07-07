b48684f01454308ad16615ee6574a7ff1dfded3f
koesnam <76582376+10kseok@users.noreply.github.com>
2025-07-08 00:43:42 +0900

    chore: SEO 최적화 적용

--- app/view/page.py
+++ app/view/page.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
@@ -10,10 +9,18 @@ templates = Jinja2Templates(directory="./app/view/templates")
 async def favicon():
     return FileResponse("app/static/assets/favicon.ico")
 
+@router.get("/sitemap.xml", include_in_schema=False)
+async def sitemap():
+    return FileResponse("app/static/assets/sitemap.xml")
+
+@router.get("/robots.txt", include_in_schema=False)
+async def robots():
+    return FileResponse("app/static/assets/robots.txt")
+
 @router.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@router.get("/convert", response_class=HTMLResponse)
+@router.get("/convert-page", response_class=HTMLResponse)
 def convert(request: Request):
     return templates.TemplateResponse("convert.html", {"request": request})
